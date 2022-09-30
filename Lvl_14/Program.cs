4dc7ed9003ac608886f62b0948773436dcc3d46d
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-01 01:51:34 +0300

    Добавлены папки 15 и 16. Сделана задача 14

--- Lvl_14/Program.cs
+++ Lvl_14/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string PairDiv(int one)
+{
+    if (one % 7 == 0 & one % 23 == 0) return "Да";
+    else return "Нет";
+}
+string result = PairDiv(num);
+Console.WriteLine(result);
