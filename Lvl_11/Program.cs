416b877a66dbee6ffb0f7b8cd832c33d4ba762c8
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-01 01:36:21 +0300

    Сделаны задачки 11 и 12

--- Lvl_11/Program.cs
+++ Lvl_11/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 11. Напишите программу, которая
+// 1. Выводит случайное трёхзначное число
+// 2. Удаляет вторую цифру этого числа
+// 3. Выводи на печать двухзначное число
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"Случайное число в интервале отрезка 10 - 99 -> {number}");
+
+int CutNum(int digNum)
+{
+    int firstNum = digNum / 100;
+    Console.WriteLine($"Первая цифра {firstNum}");
+    int thirdNum = digNum % 10;
+    Console.WriteLine($"Третья цифра {thirdNum}");
+    int resNum = firstNum * 10 + thirdNum;
+    return resNum;
+}
+int cuter = CutNum(number);
+Console.WriteLine(cuter);
--- Lvl_12/Program.cs
+++ Lvl_12/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 12. Напишите программу которая будет
+// 1. принимать на вход два числа
+// 2. выводить является ли второе число кратным первому
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+Console.Write("Ваше первое число: ");
+int fnum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ваше второе число: ");
+int snum = Convert.ToInt32(Console.ReadLine());
+
+string DivNum(int firstNUM, int secNUM)
+{
+int resNum = firstNUM % secNUM;
+if (resNum == 0) return "Кратно";
+else return $"Не кратно, остаток {resNum}";
+}
+
+string kratnoe = DivNum(fnum, snum);
+Console.WriteLine(kratnoe);
