b044c412308859308757acc5b4262d2e58943d6d
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-01 03:16:16 +0300

    Сделана задача 14 и 16

--- Lvl_14/Program.cs
+++ Lvl_14/Program.cs
@@ -1,10 +1,16 @@
-﻿Console.Write("Ваше число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Ваше число для проверки кратности: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
 
-string PairDiv(int one)
+Console.Write("Первое число для проверки: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Второе число для проверки: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+string PairDiv(int one, int two, int three)
 {
-    if (one % 7 == 0 & one % 23 == 0) return "Да";
+    if (one % two == 0 & one % three == 0) return "Да";
     else return "Нет";
 }
-string result = PairDiv(num);
+string result = PairDiv(numMain, num1, num2);
 Console.WriteLine(result);
--- Lvl_16/Program.cs
+++ Lvl_16/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 16. Напишите программу, которая
+// 1. принимает на вход два числа и проверяет
+// 2.является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Первое число для проверки: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Второе число для проверки: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool SquareNum(int numerFirst, int numerSecond)
+{
+    return numerFirst * numerFirst == numerSecond;
+}
+
+bool DivSquare(int numerFirst, int numerSecond)
+{
+    return numerSecond / numerFirst == numerFirst;
+}
+
+string Result(int n1, int n2)
+{
+    if (SquareNum(n1, n2) & DivSquare(n1, n2)) return "Да";
+    else return "Нет";
+}
+
+string proverkaNum = Result(num1, num2);
+Console.WriteLine(proverkaNum);
